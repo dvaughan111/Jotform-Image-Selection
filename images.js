e46c54a18ddfacfb7fbecf819340c037d1e6a50f
dvaughan111 <111092218+dvaughan111@users.noreply.github.com>
2025-07-21 01:22:21 -0700

    Add files via upload

---
+++ images.js
@@ -0,0 +1,4 @@
+[
+    { "id": "item1", "src": "https://github.com/dvaughan111/Jotform-Image-Selection/blob/2a15bc79629f0b4f7bc429bc022291e35da78bbb/images/Baby%20and%20Me.jpg": "Small Box" },
+    { "id": "item2", "src": "https://github.com/dvaughan111/Jotform-Image-Selection/blob/2a15bc79629f0b4f7bc429bc022291e35da78bbb/images/Charcutier%20II.jpeg", "description": "Medium Box" },
+]
